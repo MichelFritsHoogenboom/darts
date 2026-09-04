a8647b8d48b1e1915a43dff441d0270f0b11e713
Michel Frits Hoogenboom <michel@hoogenboom.io>
2026-09-04 19:17:32 +0200

    fix summarycard component standards

--- components/ui/SummaryCard.vue
+++ components/ui/SummaryCard.vue
@@ -1,22 +1,23 @@
 <script setup lang="ts">
-withDefaults(
-  defineProps<{
-    to?: string;
-    wrapperClass?: string;
-  }>(),
-  {
-    wrapperClass: "",
-  },
-);
+const { wrapperClass = "" } = defineProps<{
+  to?: string;
+  wrapperClass?: string;
+}>();
 </script>
 
 <template>
   <component
     :is="to ? 'NuxtLink' : 'div'"
     :to="to"
-    class="summary-card stat-well flex flex-col gap-2 py-2 px-6 border border-gray-700 shadow-sm hover:shadow-md transition-shadow duration-200"
+    class="summary-card stat-well"
     :class="wrapperClass"
   >
     <slot />
   </component>
 </template>
+
+<style scoped lang="postcss">
+.summary-card {
+  @apply flex flex-col gap-2 py-2 px-6 border border-gray-700 shadow-sm hover:shadow-md transition-shadow duration-200;
+}
+</style>
