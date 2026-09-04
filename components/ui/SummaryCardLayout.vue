6e8a5ccbe433c1330e64b14f37e4fff5d4253dad
Michel Frits Hoogenboom <michel@hoogenboom.io>
2026-09-04 19:12:42 +0200

    fix summarycardlayout component standards

--- components/ui/SummaryCardLayout.vue
+++ components/ui/SummaryCardLayout.vue
@@ -1,30 +1,33 @@
 <script setup lang="ts">
-withDefaults(
-  defineProps<{
-    to?: string;
-    wrapperClass?: string;
-  }>(),
-  {
-    wrapperClass: "",
-  },
-);
+const { wrapperClass = "" } = defineProps<{
+  to?: string;
+  wrapperClass?: string;
+}>();
 </script>
 
 <template>
   <UiSummaryCard :to="to" :wrapper-class="wrapperClass">
-    <h3
-      class="grid grid-cols-[20%_1fr_20%] font-bold flex justify-between items-center"
-    >
+    <h3 class="header">
       <div>
         <slot name="left" />
       </div>
       <div>
         <slot name="center" />
       </div>
-      <div class="flex items-center gap-2 justify-self-end">
+      <div class="actions">
         <slot name="actions" />
       </div>
     </h3>
     <slot />
   </UiSummaryCard>
 </template>
+
+<style scoped lang="postcss">
+.header {
+  @apply grid grid-cols-[20%_1fr_20%] font-bold flex justify-between items-center;
+}
+
+.actions {
+  @apply flex items-center gap-2 justify-self-end;
+}
+</style>
